83213b728e6ab024649eb758c75444b9219bfd63
Edward Barak <edward.barak@gmail.com>
2018-07-17 13:08:54 -0700

    Update global.sh

--- nordvpn/global.sh
+++ nordvpn/global.sh
@@ -1,6 +1,25 @@
-# random global nordVPN
-# service: nordvpn
+# random global nordvpn
+service = nordvpn
 # prompt username
+read -p 'Username:' username
+
 # prompt password
-# select random nordVPN TCP server
+read -p 'Password:' password
+
+# select random nordvpn TCP server
 # ovpn path is /etc/openvpn/ovpn_tcp
+servers = ls -l /etc/openvpn/ovpn_tcp | grep $service
+servers_count = cat $servers | wc -l
+# pick random server in $servers
+index = $RANDOM % $servers_count + 1
+# TODO: select server using servers[$index]
+# vpnFile = servers[$index] 
+vpnFile = sed -n $index'p' < $servers    
+
+# run openvpn
+openvpn $vpnFile # TODO: add username and password flags     
+
+
+
+    
+
